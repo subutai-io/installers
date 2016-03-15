42955684382f3ac544c0a78c10606e41eab77c88
dawnbreather <dawnbreather@gmail.com>
2016-03-15 15:13:27 +0600

    - fixed number of bugs
    - added checks for Framework v4, Powershell v3
    - added checks for VT-X AMD-V enabled or not
    - added pop-up forms
    - added checks for the repo
    - added checks for internet availability
    - added checks for supported platforms: Win 7,8,10 x64 only
    - added checks for administrative privileges

--- windows/bin/pre-deploy.bat
+++ windows/bin/pre-deploy.bat
@@ -1,5 +1,5 @@
 @echo off
-set SUBUTAI=c:\subutai
+set SUBUTAI=%6
 set PATH=%PATH%;%SystemDrive%\Program Files\Oracle\VirtualBox;%SUBUTAI%\bin
 cd %SUBUTAI%\bin
 
@@ -8,10 +8,12 @@ attrib -s -h deploy.bat
 call:DoReplace "powershell" "::powershell" deploy.bat deploy.bat
 call:DoReplace "::::" "::" deploy.bat deploy.bat
 
-REM echo powershell -ExecutionPolicy ByPass -Command ./autodeploy.ps1 -params deploy-redist,prepare-nat,import-ovas,clone-vm,deploy-p2p,prepare-ssh,deploy-templates -repoUrl %1 -network_installation $true -peer %2 >> deploy.bat
-REM echo powershell -ExecutionPolicy ByPass -Command ./autodeploy.ps1 %1 >> deploy.bat
-echo powershell -ExecutionPolicy ByPass -Command ./autodeploy.ps1 -params %1 -repoUrl %2 -network_installation %3 -peer %4 >> deploy.bat
-echo >> deploy.bat
+echo @echo off > deploy.bat
+echo set SUBUTAI=%6 >> deploy.bat
+echo cd %SUBUTAI%\bin >> deploy.bat
+::echo set PATH=%PATH%;%SystemDrive%\Program Files\Oracle\VirtualBox;%SUBUTAI%\bin >> deploy.bat
+echo powershell -ExecutionPolicy ByPass -Command ./autodeploy.ps1 -params %1 -repoUrl %2 -network_installation %3 -peer %4 -services %5 -appdir %6 >> deploy.bat
+::echo ./autodeploy.exe -params %1 -repoUrl %2 -network_installation %3 -peer %4 -services %5 -appdir %6 >> deploy.bat
 
 attrib +s +h deploy.bat
 attrib +s +h pre-deploy.bat
--- windows/bin/uninstall-clean.bat
+++ windows/bin/uninstall-clean.bat
@@ -1,3 +1,8 @@
 @echo off
-cd %SUBUTAI%/bin
-powershell -ExecutionPolicy ByPass -File ./autodeploy.ps1 -params clean-after-uninstall
+cd %SUBUTAI%
+
+powershell -ExecutionPolicy ByPass -Command ./bin/autodeploy.ps1 -params clean-after-uninstall
+net stop "Subutai Social P2P"
+nssm remove "Subutai Social P2P" confirm
+FOR /D %%p IN ("%SUBUTAI%\*.*") DO rmdir "%%p" /s /q > nul 2>&1
+exit 0
