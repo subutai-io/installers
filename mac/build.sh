7f462edd7256bd87d1795e419f9eb4e5345eed7d
Marat Bediev <bediev@gmail.com>
2016-04-28 17:43:49 +0600

    prepare for jenskins autobuild

--- mac/build.sh
+++ mac/build.sh
@@ -1,7 +1,9 @@
-( cd root && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/mh.pkg/Payload
-( cd root && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/rh.pkg/Payload
+#!/bin/bash
+
+( cd root.rh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/rh.pkg/Payload
+( cd root.mh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/mh.pkg/Payload
 ( cd scripts.rh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/rh.pkg/Scripts
 ( cd scripts.mh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/mh.pkg/Scripts
 mkbom -u 0 -g 80 root flat/rh.pkg/Bom
 mkbom -u 0 -g 80 root flat/mh.pkg/Bom
-( cd flat && xar --compression none -cf "../Subutai Net Installer.pkg" * )
+( cd flat && xar --compression none -cf "$@" * )
