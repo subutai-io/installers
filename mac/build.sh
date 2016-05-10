61104ba56a1f662748a9ba3717b4cdda3e6de9aa
Marat Bediev <bediev@gmail.com>
2016-05-10 15:18:58 +0600

    [mac] fix path to root folders

--- mac/build.sh
+++ mac/build.sh
@@ -4,6 +4,6 @@
 ( cd root.mh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/mh.pkg/Payload
 ( cd scripts.rh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/rh.pkg/Scripts
 ( cd scripts.mh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/mh.pkg/Scripts
-mkbom -u 0 -g 80 root flat/rh.pkg/Bom
-mkbom -u 0 -g 80 root flat/mh.pkg/Bom
+mkbom -u 0 -g 80 root.rh flat/rh.pkg/Bom
+mkbom -u 0 -g 80 root.mh flat/mh.pkg/Bom
 ( cd flat && xar --compression none -cf "$@" * )
