4fab64d38ee3e27506dcb4d6c4174eb5832dc0e5
Marat Bediev <bediev@gmail.com>
2016-06-16 16:45:26 +0600

    [mac] p2p and tray moved to separeted pkg

--- mac/build.sh
+++ mac/build.sh
@@ -2,8 +2,11 @@
 
 ( cd root.rh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/rh.pkg/Payload
 ( cd root.mh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/mh.pkg/Payload
+( cd root.services && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/services.pkg/Payload
 ( cd scripts.rh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/rh.pkg/Scripts
 ( cd scripts.mh && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/mh.pkg/Scripts
+( cd scripts.services && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/services.pkg/Scripts
 mkbom -u 0 -g 80 root.rh flat/rh.pkg/Bom
 mkbom -u 0 -g 80 root.mh flat/mh.pkg/Bom
+mkbom -u 0 -g 80 root.services flat/services.pkg/Bom
 ( cd flat && xar --compression none -cf "$@" * )
