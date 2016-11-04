d8aef729b8c55657b2e841a1028ed57fb9beeeee
btemir <vgaidamako@optimal-dynamics.com>
2016-11-05 04:27:11 +0600

    Issue #94. Third-party installer changed from VS Installer Project to Inno setup

--- windows/codebase/vs_uninstall/uninstall_clean/uninstall_clean/RG.cs
+++ windows/codebase/vs_uninstall/uninstall_clean/uninstall_clean/RG.cs
@@ -13,9 +13,11 @@ namespace uninstall_clean
         {
             //user environment
             string SubutaiProdName = "Subutai";
+            string SubutaiProdID = "{D8AEAA94-0C20-4F7E-A106-4E9617A3D7B9}_is1";
             string SubutaiVendor = "Subutai Social";
             string SubutaiTrayVendor = "Optimal-dynamics";
             string SubutaiTrayKeyName = "SS_Tray";
+            string SubutaiE2E = "kpmiofpmlciacjblommkcinncmneeoaa";
             //label1.Text = "Cleanig Registry";
             string subkey;
             RegistryKey rk;
@@ -30,6 +32,7 @@ namespace uninstall_clean
             //HKEY_CLASSES_ROOT\Installer\Products\9990BF19B70441847BF1D18B0D97D968
             subkey = "Installer\\Installer\\Products";
             DeleteKeyByValue(subkey, "Subutai", RegistryHive.ClassesRoot);
+            clean.UpdateProgress(20);
 
             //HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Products\9990BF19B70441847BF1D18B0D97D968
             subkey = "SOFTWARE\\Classes\\Installer\\Products";
@@ -40,17 +43,26 @@ namespace uninstall_clean
             //HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Subutai
             subkey = "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
             DeleteSubKeyTree(SubutaiProdName, subkey, RegistryHive.LocalMachine);
-            clean.UpdateProgress(20);
+            clean.UpdateProgress(40);
+
+            //Removing registration string
+            //HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall
+            //HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\{D8AEAA94-0C20-4F7E-A106-4E9617A3D7B9}_is1
+            //"C:\Subutai\unins000.exe" / SILENT  - uninstal string
+
+            subkey = "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+            DeleteSubKeyTree(SubutaiProdID, subkey, RegistryHive.LocalMachine);
+            clean.UpdateProgress(50);
 
             //HKLM\SOFTWARE\Wow6432Node\Subutai Social\Subutai
             subkey = "SOFTWARE\\Wow6432Node";
             DeleteSubKeyTree(SubutaiVendor, subkey, RegistryHive.LocalMachine);
-            clean.UpdateProgress(40);
+            clean.UpdateProgress(60);
 
             //HKCU\Software\Subutai Social\Subutai
             subkey = "SOFTWARE";
             DeleteSubKeyTree(SubutaiVendor, subkey, RegistryHive.CurrentUser);
-            clean.UpdateProgress(80);
+            clean.UpdateProgress(70);
 
             //Optimal-dynamics key for SubutayTray
             //HKEY_CURRENT_USER\Software\Optimal-dynamics
@@ -59,6 +71,14 @@ namespace uninstall_clean
             DeleteSubKeyTree(SubutaiTrayKeyName, subkey, RegistryHive.CurrentUser);
             subkey = "Software";
             DeleteSubKeyTree(SubutaiTrayVendor, subkey, RegistryHive.CurrentUser);
+            clean.UpdateProgress(80);
+            
+            //Google E2E plugin
+            //HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Google\Chrome\Extensions\kpmiofpmlciacjblommkcinncmneeoaa
+            subkey = "SOFTWARE\\Wow6432Node\\Google\\Chrome\\Extensions";
+            rk = Registry.LocalMachine.OpenSubKey(subkey, true);
+            DeleteSubKeyTree(SubutaiE2E, subkey, RegistryHive.LocalMachine);
+            
             clean.UpdateProgress(100);
         }
 
