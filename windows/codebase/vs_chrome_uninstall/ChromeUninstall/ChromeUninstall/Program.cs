66f2b0675e1706e4cbb317c22d84b0422ad0765c
btemir <vgaidamako@optimal-dynamics.com>
2016-12-21 05:01:35 +0600

    Issue #98. Created uninstaller for Google Crome for cases when it can not be found in Control Panel

---
+++ windows/codebase/vs_chrome_uninstall/ChromeUninstall/ChromeUninstall/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChromeUninstall
+{
+    static class Program
+    {
+        public static fChromeUninstall form1;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form1 = new fChromeUninstall();
+            Application.Run(form1);
+        }
+    }
+}
